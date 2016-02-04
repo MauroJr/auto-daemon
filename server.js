05ed21837938e31fcb3fe202b796459c4a5e9210
James Halliday <mail@substack.net>
2016-02-03 16:45:31 -0800

    force exit not necessary

--- server.js
+++ server.js
@@ -33,14 +33,9 @@ module.exports = function (createIface, opts) {
                 setTimeout(function () {
                     if (connected !== 0) return
                     server.close();
-                    if (process._getActiveHandles().length > 0) {
-                        setTimeout(forceExit, 1000);
-                    }
                 }, 1000);
             }
         }
     });
     return server;
 };
-
-function forceExit () { process.exit(1) }
